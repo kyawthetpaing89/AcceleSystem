f8e45bf8363d01e58405f77de42f3851953d7519
Kyaw Thet Paing <kyawthetpaing.mm@gmail.com>
2020-07-03 09:24:37 +0630

    ktp

--- AcceleSystem/Controllers/HomeController.cs
+++ AcceleSystem/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace AcceleSystem.Controllers
             return View();
         }
 
+        public ActionResult Index1()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
