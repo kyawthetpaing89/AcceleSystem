1d432cece1173ec79780699c15b7c9592a6b6d70
Kyaw Thet Paing <kyawthetpaing.mm@gmail.com>
2020-07-06 13:16:10 +0630

    ktp

---
+++ UserBL/User_BL.cs
@@ -0,0 +1,18 @@
+﻿using Models;
+using DL;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace UserBL
+{
+    public class User_BL
+    {
+        public string UserLogin_Select(UserModel Umodel)
+        {
+            BaseDL bdl = new BaseDL();
+            SqlParameter[] prms = new SqlParameter[3];
+            prms[0] = new SqlParameter("@UserID", SqlDbType.VarChar) { Value = Umodel.UserID };
+            prms[1] = new SqlParameter("@Password", SqlDbType.VarChar) { Value = Umodel.Password };
+        }
+    }
+}
