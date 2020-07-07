0b3ab615241d7a7d8a640b37f7cfa11a6d65aae0
KhinMyoMyatThaw <58497238+KhinMyoMyatThaw@users.noreply.github.com>
2020-07-07 17:21:28 +0630

    by Thaw Thaw

--- UserBL/User_BL.cs
+++ UserBL/User_BL.cs
@@ -16,5 +16,24 @@ namespace UserBL
 
             return bdl.SelectJson("UserLogin_Select", prms);
         }
+        public string User_Select(UserModel Umodel)
+        {
+            BaseDL bdl = new BaseDL();
+            SqlParameter[] prms = new SqlParameter[2];
+            prms[0] = new SqlParameter("@UserID", SqlDbType.VarChar) { Value = Umodel.UserID };
+            prms[1] = new SqlParameter("@UserName", SqlDbType.VarChar) { Value = Umodel.UserName };
+
+            return bdl.SelectJson("User_Select", prms);
+        }
+        public string User_Insert(UserModel Umodel)
+        {
+            BaseDL bdl = new BaseDL();
+            SqlParameter[] prms = new SqlParameter[3];
+            prms[0] = new SqlParameter("@UserID", SqlDbType.VarChar) { Value = Umodel.UserID };
+            prms[1] = new SqlParameter("@UserName", SqlDbType.VarChar) { Value = Umodel.UserName };
+            prms[2] = new SqlParameter("@Password", SqlDbType.VarChar) { Value = Umodel.Password };
+
+            return bdl.SelectJson("User_Insert", prms);
+        }
     }
 }
