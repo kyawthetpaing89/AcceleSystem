0bebce1d8c8cf0958a10054ecaeb27741b70f878
Kyaw Thet Paing <kyawthetpaing.mm@gmail.com>
2020-07-13 18:18:46 +0630

    ktp

---
+++ AcceleSystem/ElaAdmin/assets/js/Message.js
@@ -0,0 +1,21 @@
+﻿function GetMessage(MessageID,url) {
+    var Mmodel = {
+        MessageID: MessageID,
+    };
+    return $.ajax({
+        url: url,
+        method: 'Post',
+        dataType: 'json',
+        contentType: 'application/json; charset=utf-8',
+        data: JSON.stringify(Mmodel),
+        headers:
+        {
+            Authorization: 'Basic ' + btoa('Capital_MM' + ':' + 'CKM12345!')
+        },
+    });
+}
+
+function ShowMessage(data) {
+    
+}
+
