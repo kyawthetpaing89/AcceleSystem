0a31f8da6527224b197fc39525f743c55d13ef8e
KhinMyoMyatThaw <58497238+KhinMyoMyatThaw@users.noreply.github.com>
2020-07-14 09:24:06 +0630

    By Thaw Thaw

---
+++ Models/BrandModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class BrandModel : BaseModel
+    {
+        public string BrandCD { get; set; }
+        public string BrandName { get; set; }
+       
+    }
+}
