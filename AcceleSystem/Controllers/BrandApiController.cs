a7db7bb97fefd3abc71f0409f023cf10858c9223
KhinMyoMyatThaw <58497238+KhinMyoMyatThaw@users.noreply.github.com>
2020-07-14 15:17:39 +0630

    By Thaw Thaw

--- AcceleSystem/Controllers/BrandApiController.cs
+++ AcceleSystem/Controllers/BrandApiController.cs
@@ -7,13 +7,24 @@ namespace AcceleSystem.Controllers
 {
     public class BrandApiController : ApiController
     {
+        [UserAuthentication]
+        [HttpPost]
+        public string M_Brand_Select([FromBody] BrandModel Umodel)
+        {
+            if (Umodel == null)
+            {
+                Umodel = new BrandModel();
+            }
+            Brand_BL Ubl = new Brand_BL();
+            return Ubl.M_Brand_Select(Umodel);
+        }
 
         [UserAuthentication]
         [HttpPost]
-        public string M_Brand_Insert([FromBody] BrandModel Umodel)
+        public string Brand_CUD([FromBody] BrandModel Umodel)
         {
             Brand_BL Ubl = new Brand_BL();
-            return Ubl.M_Brand_Insert(Umodel);
+            return Ubl.Brand_CUD(Umodel);
         }
     }
 }
