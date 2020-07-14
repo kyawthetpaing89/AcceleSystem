44d52a5842b45a9dd3d759fee24a249574cb5398
Pann Nu Zan <Pann Nu Zan@DESKTOP-52825H4>
2020-07-14 17:19:25 +0630

    pnz

--- AcceleSystem/Controllers/KanagataController.cs
+++ AcceleSystem/Controllers/KanagataController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Models;
 
 namespace AcceleSystem.Controllers
 {
     public class KanagataController : Controller
     {
         // GET: Kanagata
-        public ActionResult KanagataEntry()
+        public ActionResult KanagataEntry(KanagataModel kgmodel)
         {
+            if (string.IsNullOrWhiteSpace(kgmodel.Mode))
+                kgmodel.Mode = "New";
             return View();
         }
 
