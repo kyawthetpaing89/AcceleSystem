6a3821f0afd51469a214eafdc448eef53eae507a
KhinMyoMyatThaw <capital.khinmyomyatthaw@gmail.com>
2020-07-23 08:28:40 +0630

    By Thaw Thaw

---
+++ AcceleSystem/Controllers/MasterTouroku_KeihiSetteiController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AcceleSystem.Controllers
+{
+    public class MasterTouroku_KeihiSetteiController : Controller
+    {
+        // GET: MasterTouroku_KeihiSettei
+        public ActionResult MasterTouroku_KeihiSetteiList()
+        {
+            return View();
+        }
+    }
+}
---
+++ MasterTouroku_KeiSetteiBL/MasterTouroku_KeiSettei_BL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterTouroku_KeiSetteiBL
+{
+    public class MasterTouroku_KeiSettei_BL
+    {
+        public string M_Keihi_Select(KeihiModel Kmodel)
+        {
+            BaseDL bdl = new BaseDL();
+            bmodel.Sqlprms = new SqlParameter[2];
+            bmodel.Sqlprms[0] = new SqlParameter("@BrandCD", SqlDbType.VarChar) { Value = bmodel.BrandCD };
+            bmodel.Sqlprms[1] = new SqlParameter("@BrandName", SqlDbType.VarChar) { Value = bmodel.BrandName };
+
+            return bdl.SelectJson("M_Brand_Select", bmodel.Sqlprms);
+        }
+    }
+}
