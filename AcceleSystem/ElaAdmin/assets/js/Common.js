64687d33b861cec840a26585e58e3942e27cdec4
Saw Yupar Kyaw <capital-sawyuparkyaw>
2020-08-07 17:40:48 +0630

    By Sawlay

--- AcceleSystem/ElaAdmin/assets/js/Common.js
+++ AcceleSystem/ElaAdmin/assets/js/Common.js
@@ -68,7 +68,7 @@ function ShowSuccessMessage(msgdata,url) {
         title: message[0].MessageID,
         text: message[0].MessageText1,
     }).then(function () {
-        if (message[0].status != "success")
+        if (message[0].status != "error")
             location.href = url;
     });
 }
@@ -145,7 +145,11 @@ function ErrChk(ctrl) {
                     }
                 }
                 else {
-                    return BrandData[0].MessageID;
+                    if ($(ctrl).attr("data-NameCtrl")) {
+                        var ctrlName = $(ctrl).attr("data-NameCtrl");
+                        $('#' + ctrlName).val("");
+                        return BrandData[0].MessageID;
+                    }
                 }
                 break;
         }
@@ -158,6 +162,32 @@ function ErrChk(ctrl) {
             case "Casting":
                 var model = {
                     CastingCD: $(ctrl).val()
+                };
+                var data = CalltoApiController(ApiURL, model);
+                var BrandData = JSON.parse(data);
+                if (BrandData[0].MessageID != "E107") {
+                    return "0";
+                }
+                else {
+                    return BrandData[0].MessageID;
+                }
+                break;
+            case "User":
+                var model = {
+                    UserID: $(ctrl).val()
+                };
+                var data = CalltoApiController(ApiURL, model);
+                var UserData = JSON.parse(data);
+                if (UserData[0].MessageID != "E107") {
+                    return "0";
+                }
+                else {
+                    return UserData[0].MessageID;
+                }
+                break;
+            case "Brand":
+                var model = {
+                    BrandCD: $(ctrl).val()
                 };
                 var data = CalltoApiController(ApiURL, model);
                 var BrandData = JSON.parse(data);
