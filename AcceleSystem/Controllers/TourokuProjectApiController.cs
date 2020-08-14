5593d9ad140dc578c4e2106a43ac6aa9a383eb33
KhinMyoMyatThaw <capital.khinmyomyatthaw@gmail.com>
2020-08-14 13:25:56 +0630

    By Thaw Thaw

---
+++ AcceleSystem/Controllers/TourokuProjectApiController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Http;
+using System.Web.Http.Results;
+using Models;
+using TourokuBL;
+
+namespace AcceleSystem.Controllers
+{
+    public class TourokuProjectApiController : ApiController
+    {
+        [UserAuthentication]
+        [HttpPost]
+        public string M_Project_Select_List([FromBody] TourokuModel Tmodel)
+        {
+            if (Tmodel == null)
+            {
+                Tmodel = new TourokuModel();
+            }
+            Touroku_BL Ubl = new Touroku_BL();
+            return Ubl.M_Project_Select_List(Tmodel);
+        }
+    }
+}
