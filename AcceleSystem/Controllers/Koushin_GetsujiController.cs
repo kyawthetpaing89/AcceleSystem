24ed4df342339b6102bdc9a042f4bb9f97b0cb4a
Pann Nu Zan <Pann Nu Zan@DESKTOP-52825H4>
2020-08-14 08:31:15 +0630

    pnz

---
+++ AcceleSystem/Controllers/Koushin_GetsujiController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AcceleSystem.Controllers
+{
+    public class Koushin_GetsujiController : Controller
+    {
+        // GET: Koushin_Getsuji
+        public ActionResult Koushin_Getsuji()
+        {
+            return View();
+        }
+    }
+}
