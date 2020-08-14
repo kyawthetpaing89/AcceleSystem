a52b2ab4d533a19fe0f8da5a627bbc9d283c466a
Pann Nu Zan <Pann Nu Zan@DESKTOP-52825H4>
2020-08-14 10:38:36 +0630

    By pnz

--- AcceleSystem/Controllers/Koushin_GetsujiApiController.cs
+++ AcceleSystem/Controllers/Koushin_GetsujiApiController.cs
@@ -1,16 +1,16 @@
 ﻿using System.Web.Http;
 using Models;
-using Koushin_GetsujiBL;
+//using Koushin_GetsujiBL;
 
 namespace AcceleSystem.Controllers
 {
     public class Koushin_GetsujiApiController : ApiController
     {
         // GET: Koushin_GetsujiApi
-        public string M_Contrl_YearMonth_ExitCheck([FromBody] Koushin_GetsujiModel kgmodel)
-        {
-            Koushin_Getsuji_BL kgbl = new Koushin_Getsuji_BL();
-            return kgbl.M_Contrl_YearMonth_ExitCheck(kgmodel);
-        }
+        //public string M_Contrl_YearMonth_ExitCheck([FromBody] Koushin_GetsujiModel kgmodel)
+        //{
+        //    /Koushin_Getsuji_BL kgbl = new Koushin_Getsuji_BL();
+        //    return kgbl.M_Contrl_YearMonth_ExitCheck(kgmodel);
+        //}
     }
 }
