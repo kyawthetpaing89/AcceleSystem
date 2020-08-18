50e44308372b20e502abc40de2753d606c300d22
Pann Nu Zan <Pann Nu Zan@DESKTOP-52825H4>
2020-08-18 17:35:22 +0630

    pnz

---
+++ AcceleSystem/Controllers/Touroku_KeihiController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AcceleSystem.Controllers
+{
+    public class Touroku_KeihiController : Controller
+    {
+        // GET: Touroku_Keihi
+        public ActionResult Touroku_KeihiEntry()
+        {
+            return View();
+        }
+
+        public ActionResult Touroku_KeihiList()
+        {
+            return View();
+        }
+    }
+}
