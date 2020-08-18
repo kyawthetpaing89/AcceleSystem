0886475bed3a1eba20896486eb368e915e20db15
Saw Yupar Kyaw <capital-sawyuparkyaw>
2020-08-18 17:30:06 +0630

    By Sawlay

---
+++ Models/TourokuNouhinModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+     public class TourokuNouhinModel : BaseModel
+     {
+        public string SEQ { get; set; }
+        public string DeliveryDate { get; set; }
+        public string BrandCD { get; set; }
+        public string BrandName { get; set; }
+        public string Season { get; set; }
+        public string Year { get; set; }
+        public string ProjectCD { get; set; }
+        public string ProjectName { get; set; }
+        public string HinbanCD { get; set; }
+        public string HinbanName { get; set; }
+        public string Production { get; set; }
+        public string DeliveryAmount { get; set; }
+    }
+}
