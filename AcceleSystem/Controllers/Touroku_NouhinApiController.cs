285a7e5b4913adff2a3a4ac829cc7f1bb5f05b07
KhinMyoMyatThaw <capital.khinmyomyatthaw@gmail.com>
2020-08-19 11:03:27 +0630

    By Thaw Thaw

--- AcceleSystem/Controllers/Touroku_NouhinApiController.cs
+++ AcceleSystem/Controllers/Touroku_NouhinApiController.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Models;
+using Touroku_NouhinBL;
 
 namespace AcceleSystem.Controllers
 {
     public class Touroku_NouhinApiController : Controller
     {
-        // GET: Touroku_NouhinApi
-      
+        [UserAuthentication]
+        [HttpPost]
+        //public string M_Casting_Select([FromBody] KanagataModel kgmodel)
+        //{
+        //    Kanagata_BL kgbl = new Kanagata_BL();
+        //    return kgbl.M_Casting_Select(kgmodel);
+        //}
     }
 }
---
+++ Models/Koushin_GenkaModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Koushin_GenkaModel: BaseModel
+    {
+        public string processing_date { get; set; }
+    }
+}
