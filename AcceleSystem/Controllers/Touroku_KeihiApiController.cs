30223530a304686d0c13f8462bcf7811f16ede1e
Pann Nu Zan <Pann Nu Zan@DESKTOP-52825H4>
2020-08-19 10:40:24 +0630

    pnz

---
+++ AcceleSystem/Controllers/Touroku_KeihiApiController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Http;
+using Models;
+using KanagataBL;
+
+namespace AcceleSystem.Controllers
+{
+    public class Touroku_KeihiApiController : ApiController
+    {
+        // GET: Touroku_KeihiApi
+        public string M_Keihi_ExistsCheck([FromBody] KanagataModel kgmodel)
+        {
+            Kanagata_BL kgbl = new Kanagata_BL();
+            return kgbl.M_Casting_ExistsCheck(kgmodel);
+        }
+    }
+}
