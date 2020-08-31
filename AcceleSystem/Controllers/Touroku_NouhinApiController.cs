e4621c8b6990be5325dead1423fc7470a6dbb006
Saw Yupar Kyaw <capital-sawyuparkyaw>
2020-08-31 10:07:13 +0630

    By Sawlay

--- AcceleSystem/Controllers/Touroku_NouhinApiController.cs
+++ AcceleSystem/Controllers/Touroku_NouhinApiController.cs
@@ -14,5 +14,13 @@ namespace AcceleSystem.Controllers
             Touroku_Nouhin_BL tnbl = new Touroku_Nouhin_BL();
             return tnbl.D_Delivery_Search(Tnmodel);
         }
+
+        [UserAuthentication]
+        [HttpPost]
+        public string M_Project_Nouhin_ShowData([FromBody] TourokuNouhinModel Tnmodel)
+        {
+            Touroku_Nouhin_BL tnbl = new Touroku_Nouhin_BL();
+            return tnbl.M_Project_Nouhin_ShowData(Tnmodel);
+        }
     }
 }
