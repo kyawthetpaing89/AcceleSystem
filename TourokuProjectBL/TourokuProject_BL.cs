646e95fe91ea8cbec414798260c6a3bfae8d443a
KhinMyoMyatThaw <capital.khinmyomyatthaw@gmail.com>
2020-09-01 09:22:01 +0630

    By Thaw Thaw

--- TourokuProjectBL/TourokuProject_BL.cs
+++ TourokuProjectBL/TourokuProject_BL.cs
@@ -68,6 +68,22 @@ namespace TourokuProjectBL
            
 
             }
+            else if (Tmodel.Mode.Equals("Copy"))
+            {
+                Tmodel.SPName = "M_Project_Insert";
+                Tmodel.Sqlprms = new SqlParameter[9];
+                Tmodel.Sqlprms[0] = new SqlParameter("@ProjectCD", SqlDbType.VarChar) { Value = Tmodel.ProjectCD };
+                Tmodel.Sqlprms[1] = new SqlParameter("@ProjectName", SqlDbType.VarChar) { Value = Tmodel.ProjectName };
+                Tmodel.Sqlprms[2] = new SqlParameter("@Year", SqlDbType.Int) { Value = (object)Tmodel.Year ?? DBNull.Value };
+                Tmodel.Sqlprms[3] = new SqlParameter("@BrandCD", SqlDbType.VarChar) { Value = Tmodel.BrandCD };
+                Tmodel.Sqlprms[4] = new SqlParameter("@Season", SqlDbType.TinyInt) { Value = Tmodel.Season };
+                Tmodel.Sqlprms[5] = new SqlParameter("@PeriodStart", SqlDbType.Int) { Value = (object)Tmodel.PeriodStart ?? DBNull.Value };
+                Tmodel.Sqlprms[6] = new SqlParameter("@PeriodEnd", SqlDbType.Int) { Value = (object)Tmodel.PeriodEnd ?? DBNull.Value };
+                Tmodel.Sqlprms[7] = new SqlParameter("@ProjectManager", SqlDbType.VarChar) { Value = Tmodel.ProjectManager };
+                Tmodel.Sqlprms[8] = new SqlParameter("@AllocationCount", SqlDbType.Int) { Value = (object)Tmodel.AllocationCount ?? DBNull.Value };
+
+
+            }
             else if (Tmodel.Mode.Equals("Delete"))
             {
                 Tmodel.SPName = "M_Project_Delete";
