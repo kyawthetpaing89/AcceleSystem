caeddb64a873987e2ffbb867b247228de4232924
Nyisoe23 <capital.nyinyisoe@gmail.com>
2020-11-25 09:11:41 +0630

    by nns keihilist & nouhinlist design

--- AcceleSystem/Controllers/Touroku_KeihiController.cs
+++ AcceleSystem/Controllers/Touroku_KeihiController.cs
@@ -10,13 +10,14 @@ namespace AcceleSystem.Controllers
     public class Touroku_KeihiController : Controller
     {
         // GET: Touroku_Keihi
+        [SessionFilter]
         public ActionResult Touroku_KeihiEntry(Touroku_KeihiModel kmodel)
         {
             if (string.IsNullOrWhiteSpace(kmodel.Mode))
                 kmodel.Mode = "New";
             return View(kmodel);
         }
-
+        [SessionFilter]
         public ActionResult Touroku_KeihiList(Touroku_KeihiModel kmodel)
         {
             return View(kmodel);
