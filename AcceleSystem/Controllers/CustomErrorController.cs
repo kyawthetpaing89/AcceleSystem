3a43ffc9fa1f4b022634ae063015c2efc46433b5
Saw Yupar Kyaw <capital-sawyuparkyaw>
2021-01-28 15:57:58 +0630

    by sawlay

---
+++ AcceleSystem/Controllers/CustomErrorController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AcceleSystem.Controllers
+{
+    public class CustomErrorController : Controller
+    {
+        // GET: CustomError
+        [HandleError]
+      
+        public ActionResult CustomErrorView()
+        {
+            return View();
+        }
+    }
+}
