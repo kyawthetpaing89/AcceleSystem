d62432f57a3b78e03e3be063926d040e05399ca0
Nyisoe23 <capital.nyinyisoe@gmail.com>
2021-02-05 10:01:58 +0630

    by nns

--- Koushin_GenkaBL/Koushin_Genka_BL.cs
+++ Koushin_GenkaBL/Koushin_Genka_BL.cs
@@ -23,7 +23,7 @@ namespace Koushin_GenkaBL
             BaseDL bdl = new BaseDL();
             kgmodel.Sqlprms = new SqlParameter[0];
             //kgmodel.Sqlprms[0] = new SqlParameter("@Date", SqlDbType.VarChar) { Value = (object)kgmodel.processing_date ?? DBNull.Value };
-            return bdl.InsertUpdateDeleteData("M_Contrl_YearMonth_Genka_Update", kgmodel.Sqlprms);
+            return bdl.SelectJson("M_Contrl_YearMonth_Genka_Update", kgmodel.Sqlprms);
         }
 
     }
